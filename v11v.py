60d5bbbe877ec73317ed65027e30ff8ce777dd05
dg1an3 <dg1an3@github.com>
2023-05-06 15:07:02 -0500

    reconstruction from powermap

--- v11v.py
+++ v11v.py
@@ -31,6 +31,14 @@ class V11V(nn.Module):
             directions=directions,
         )
 
+        self.layer_3 = OrientedPowerMap(
+            in_channels=self.layer_2.out_channels,
+            use_abs=False,
+            use_batch_norm=True,
+            kernel_size=kernel_size,
+            directions=directions,
+        )
+
     def encode(self, x):
         """_summary_
 
@@ -42,7 +50,8 @@ class V11V(nn.Module):
         """
         layer_1_out = self.layer_1(x)
         layer_2_out = self.layer_2(layer_1_out)
-        return [layer_1_out, layer_2_out]
+        layer_3_out = self.layer_3(layer_2_out)
+        return [layer_1_out, layer_2_out, layer_3_out]
 
     def decode(self, x):
         """_summary_
@@ -53,9 +62,30 @@ class V11V(nn.Module):
         Returns:
             _type_: _description_
         """
-        decoder_2_out = self.layer_2.decoder(x)
-        decoder_1_out = self.layer_1.decoder(decoder_1_out)
-        return [decoder_2_out, decoder_1_out]
+        decoder_3_out = self.layer_3.decoder(x)
+        decoder_2_out = self.layer_2.decoder(decoder_3_out)
+        decoder_1_out = self.layer_1.decoder(decoder_2_out)
+        return [decoder_3_out, decoder_2_out, decoder_1_out]
+
+    def forward_dict(self, x):
+        """_summary_
+
+        Args:
+            x (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        [layer_1_out, layer_2_out, layer_3_out] = self.encode(x)
+        [decoder_3_out, decoder_2_out, decoder_1_out] = self.decode(layer_3_out)
+        return {
+            "layer_1_out": layer_1_out,
+            "layer_2_out": layer_2_out,
+            "layer_3_out": layer_3_out,
+            "decoder_3_out": decoder_3_out,
+            "decoder_2_out": decoder_2_out,
+            "decoder_1_out": decoder_1_out,
+        }
 
     def forward(self, x):
         """_summary_
@@ -66,29 +96,16 @@ class V11V(nn.Module):
         Returns:
             _type_: _description_
         """
-        [layer_1_out, layer_2_out] = self.encode(x)
-        [decoder_2_out, decoder_1_out] = self.decode(layer_2_out)
-        return [layer_1_out, layer_2_out, decoder_2_out, decoder_1_out]
+        result_dict = self.forward_dict(x)
+        return result_dict["decoder_1_out"]
 
 
 if __name__ == "__main__":
-    import numpy as np
-    import matplotlib.pyplot as plt
+    # construct the model
+    model = V11V(in_channels=1, kernel_size=11, directions=7)
 
-    # Create a random grayscale image (replace this with your actual image data)
-    # image = np.random.rand(256, 256)
-
-    import torch.nn.functional as F
-    import torch.optim as optim
-    from torch.utils.data import DataLoader
     from torchinfo import summary
 
-    from load_dataset import load_dataset
-
-    device = torch.device("cuda" if torch.cuda.is_available else "cpu")
-
-    # construct the model
-    model = V11V(input_size=(1, 448, 448), kernel_size=11, directions=7)
     print(
         summary(
             model,
@@ -108,7 +125,9 @@ if __name__ == "__main__":
     columns = 7 + 1  # directions+1
     rows = model.layer_1.conv_real.weight.shape[0] // columns
 
-    fig, ax = plt.subplots(rows, columns, figsize=(20, 8))
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(rows, columns, figsize=(10, 4))
     plt.ion()
 
     for row in range(rows):
@@ -119,13 +138,23 @@ if __name__ == "__main__":
 
     plt.show()
 
-    model = model.to(device)
+    # set up the data loader
+    from torch.utils.data import DataLoader
+    from cxr8_dataset import get_clahe_transforms, Cxr8Dataset
 
-    # perform training
-    train_dataset = load_dataset("cxr8", input_size=(1, 448, 448), clahe_tile_size=8)
+    import os
+    from pathlib import Path
+    data_temp_path = os.environ["DATA_TEMP"]
+    root_path = Path(data_temp_path) / "cxr8"
+
+    transforms = get_clahe_transforms(clahe_tile_size=8, input_size=448)    
+    train_dataset = Cxr8Dataset(root_path, transform=transforms)
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    device = torch.device("cuda" if torch.cuda.is_available else "cpu")
+    model = model.to(device)
+
+    import torch.nn.functional as F
 
     recon_loss_metric = "l1_loss"
     if recon_loss_metric == "binary_cross_entropy":
@@ -135,9 +164,14 @@ if __name__ == "__main__":
     elif recon_loss_metric == "l1_loss":
         recon_loss = F.l1_loss
     elif recon_loss_metric == "mse" or recon_loss_metric != "":
-        recon_loss = F.mse
+        recon_loss = F.mse_loss
 
-    fig, ax = plt.subplots(2, 5, figsize=(20, 8))
+    import numpy as np
+    import torch.optim as optim
+
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+    fig, ax = plt.subplots(2, 5, figsize=(10, 4))
     plt.ion()
     num_epochs = 6
 
@@ -145,25 +179,26 @@ if __name__ == "__main__":
         model.train()
         train_loss = 0
         train_count = 0
-        for batch_idx, (batch, _) in enumerate(train_loader):
-            batch = batch.to(device)
+        for batch_idx, batch_dict in enumerate(train_loader):
+            batch_images = batch_dict["image"]
+            batch_images = batch_images.to(device)
             optimizer.zero_grad()
 
-            recon_batch = model(batch)
-            loss_value = recon_loss(batch, recon_batch)
+            result_dict = model.forward_dict(batch_images)
+            recon_images = result_dict["decoder_1_out"]
+            # print(batch_images.shape)
+            # print(recon_images.shape)
+            loss_value = recon_loss(batch_images, recon_images)
+            loss_value += recon_loss(result_dict["layer_1_out"], result_dict["decoder_2_out"])
 
             if train_count % 10 == 0:
-                batch = batch.clone().cpu().detach().numpy()
-                recon_batch = recon_batch.cpu().detach().numpy()
+                batch_images = batch_images.clone().cpu().detach().numpy()
+                recon_images = recon_images.cpu().detach().numpy()
 
                 # print(v.shape)
                 for n in range(5):
-                    ax[0][n].imshow(
-                        torch.movedim(torch.tensor(batch[n]), 0, -1), cmap="bone"
-                    )
-                    ax[1][n].imshow(
-                        torch.movedim(torch.tensor(recon_batch[n]), 0, -1), cmap="bone"
-                    )
+                    ax[0][n].imshow(np.squeeze(batch_images[n]), cmap="bone")
+                    ax[1][n].imshow(np.squeeze(recon_images[n]), cmap="bone")
             plt.show()
             plt.pause(0.01)
 
