a354a276d1b6f6e860f8f8dfa8dd44878d06a3fa
dg1an3 <dg1an3@github.com>
2023-05-21 21:05:27 -0500

    demarcated stn for removal ; adding comments

--- vae.py
+++ vae.py
@@ -32,57 +32,59 @@ def clamp_01(x, eps=1e-6):
     return torch.clamp(x, eps, 1.0 - eps)
 
 
-# TODO: vae_loss kwargs
 def vae_loss(
-    recon_loss_metrics,  # (F.binary_cross_entropy,F.l1_loss)
+    recon_loss_metrics,
     beta,
-    x_recon,
     x,
     mu,
     log_var,
+    x_recon,
     v1_weight=None,
-    x_after_v1=None,
-    x_before_v1=None,
+    perc_loss=None,
+    perc_loss_recon=None,
 ):
-    """_summary_
+    """compute the total VAE loss, including reconstruction error and kullback-liebler divergence with a unit gaussian
 
     Args:
-        recon_x (_type_): _description_
-        x (_type_): _description_
-        mu (_type_): _description_
-        log_var (_type_): _description_
-        recon_loss_metric (str, optional): _description_. Defaults to 'l1_loss'.
-        beta (float, optional): _description_. Defaults to 1.0.
+        recon_loss_metrics (Tuple[Func]): example: (F.binary_cross_entropy,F.l1_loss,F.mse_loss)
+        x (torch.Tensor): original tensor target to match
+        mu (torch.Tensor): mean values for reparameterization
+        log_var (torch.Tensor): log variance for reparameterization
+        x_recon (torch.Tensor): reconstructed value to which to compare
+        v1_weight (torch.Tensor, optional): weight vector for perceptual loss. Defaults to None.
+        perc_loss (torch.Tensor, optional): upward perceptual loss features. Defaults to None.
+        perc_loss_recon (torch.Tensor, optional): reconstructed perceptual loss features. Defaults to None.
 
     Returns:
-        _type_: _description_
+        Tuple[float,float,float]: (reconstruction loss, kldiv loss, total loss)
     """
 
     x = clamp_01(x)
     x_recon = clamp_01(x_recon)
-    x_after_v1 = clamp_01(x_after_v1)
-    x_before_v1 = clamp_01(x_before_v1)
+    perc_loss = clamp_01(perc_loss)
+    perc_loss_recon = clamp_01(perc_loss_recon)
 
-    match v1_weight:
-        case None:
-            pass
-        case np.ndarray():
-            v1_weight = torch.tensor(v1_weight).to(x_after_v1.device)
-        case torch.Tensor():
-            v1_weight = v1_weight.to(x_after_v1.device)
-        case _:
-            raise ("unknown weight type")
+    if isinstance(v1_weight, np.ndarray):
+        v1_weight = torch.tensor(v1_weight).to(perc_loss.device)
+
+    if isinstance(v1_weight, torch.Tensor):
+        v1_weight = v1_weight.to(perc_loss.device)
+    else:
+        raise ("unknown weight type")
 
     recon_loss = 0.0
     for loss_func in recon_loss_metrics:
         recon_loss += loss_func(x, x_recon, reduction="mean")
-        if x_after_v1 is not None:
-            v1_loss = loss_func(
-                x_after_v1,
-                x_before_v1,
-                reduction="mean" if v1_weight is None else "none",
+        if perc_loss is not None:
+            use_weight = (
+                v1_weight is not None and loss_func is not F.binary_cross_entropy
             )
-            if v1_weight is not None:
+            v1_loss = loss_func(
+                perc_loss,
+                perc_loss_recon,
+                reduction="none" if use_weight else "mean",
+            )
+            if use_weight:
                 v1_loss = torch.mean(v1_loss, (0, -1, -2))
                 v1_loss = torch.mul(v1_weight, v1_loss)
                 v1_loss = torch.mean(v1_loss)
@@ -95,16 +97,44 @@ def vae_loss(
 
 class VAE(nn.Module):
     def __init__(self, input_size, init_kernel_size=13, latent_dim=32, use_stn=True):
-        """_summary_
+        """construct a resnet 34 VAE module
 
         Args:
-            input_size (_type_): _description_
-            init_kernel_size (int, optional): _description_. Defaults to 11.
-            latent_dim (int, optional): _description_. Defaults to 32.
+            input_size (torch.Size): input size for the model
+            init_kernel_size (int, optional): sz x sz of kernel. Defaults to 11.
+            latent_dim (int, optional): latent dimension of VAE. Defaults to 32.
         """
         super(VAE, self).__init__()
 
+        # construct a dummy input tensor
         input = torch.randn(input_size)
+
+        self.input_size = input_size
+        self.latent_dim = latent_dim
+
+        self.encoder = Encoder(
+            input_size,
+            init_kernel_size=init_kernel_size,
+            directions=7,
+            latent_dim=latent_dim,
+            use_ori_map="phased",
+            use_abs=True,
+        )
+
+        self.decoder = Decoder(
+            self.encoder.input_size_to_fc,
+            latent_dim=latent_dim,
+            out_channels=input_size[0],
+            final_kernel_size=init_kernel_size,
+        )
+
+        #######################################################################################
+        #######################################################################################
+        #     ###     ###     ###     ###     ###     ###     ###     ###
+        #       ###     ###     ###     ###     ###     ###     ###     ###
+        #     ###     ###     ###     ###     ###     ###     ###     ###
+        #######################################################################################
+        #######################################################################################
 
         self.use_stn = use_stn
         if self.use_stn:
@@ -139,24 +169,6 @@ class VAE(nn.Module):
             self.fc_loc[2].bias.data.copy_(
                 torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float)
             )
-
-        self.input_size = input_size
-        self.latent_dim = latent_dim
-
-        self.encoder = Encoder(
-            input_size,
-            init_kernel_size=init_kernel_size,
-            directions=7,
-            latent_dim=latent_dim,
-            use_ori_map="phased",
-            use_abs=True,
-        )
-        self.decoder = Decoder(
-            self.encoder.input_size_to_fc,
-            latent_dim=latent_dim,
-            out_channels=input_size[0],
-            final_kernel_size=init_kernel_size,
-        )
 
     def stn(self, x):
         """Spatial transformer network forward function
@@ -221,74 +233,107 @@ class VAE(nn.Module):
 
         return x
 
+        #######################################################################################
+        #######################################################################################
+        #     ###     ###     ###     ###     ###     ###     ###     ###
+        #   ###     ###     ###     ###     ###     ###     ###     ###
+        #     ###     ###     ###     ###     ###     ###     ###     ###
+        #######################################################################################
+        #######################################################################################
+
     def reparameterize(self, mu, log_var):
-        """_summary_
+        """perform reparameterization trick given mean and log variance
 
         Args:
-            mu (_type_): _description_
-            log_var (_type_): _description_
+            mu (torch.Tensor): mean tensor for gaussian
+            log_var (torch.Tensor): log variances for gaussian
 
         Returns:
-            _type_: _description_
+            torch.Tensor: sampled value
         """
         std = torch.exp(0.5 * log_var)
         eps = torch.randn_like(std)
         return mu + eps * std
 
     def forward_dict(self, x):
-        """_summary_
+        """perform forward pass, returning a dictionary of useful results for loss functions
 
         Args:
-            x (_type_): _description_
+            x (torch.Tensor): input vector
 
         Returns:
-            _type_: _description_
+            dictionary: dictionary of result tensors
         """
+
+        #######################################################################################
+        #######################################################################################
+        #     ###     ###     ###     ###     ###     ###     ###     ###
+        #       ###     ###     ###     ###     ###     ###     ###     ###
+        #     ###     ###     ###     ###     ###     ###     ###     ###
+        #######################################################################################
+        #######################################################################################
         # first apply the transform
         if self.use_stn:
             x = self.stn(x)
+        #######################################################################################
+        #######################################################################################
+        #     ###     ###     ###     ###     ###     ###     ###     ###
+        #   ###     ###     ###     ###     ###     ###     ###     ###
+        #     ###     ###     ###     ###     ###     ###     ###     ###
+        #######################################################################################
+        #######################################################################################
 
-        mu, log_var, x_after_v1 = self.encoder(x)
-        # print(f"x_after_v1.shape = {x_after_v1.shape}")
-        # print(f"x_after_v2.shape = {x_after_v2.shape}")
+        mu, log_var, perc_loss = self.encoder(x)
+
+        # TODO: get this from self.encoder(x)
+        phi = (5**0.5 + 1) / 2  # golden ratio
+        v1_weight = torch.Tensor(
+            list([weight for n in range(2, -3, -1) for weight in [phi ** (n * 1)] * 8])
+        )
 
         z = self.reparameterize(mu, log_var)
 
-        x_recon, x_before_v1 = self.decoder(z)
-        # print(f"x_before_v2.shape = {x_before_v2.shape}")
-        # print(f"x_before_v1.shape = {x_before_v1.shape}")
+        x_recon, perc_loss_recon = self.decoder(z)
 
         return {
             "x_recon": x_recon,
             "mu": mu,
             "log_var": log_var,
-            "x_after_v1": x_after_v1,
-            "x_before_v1": x_before_v1,
+            "perc_loss": perc_loss,
+            "perc_loss_recon": perc_loss_recon,
+            "v1_weight": v1_weight,
         }
 
     def forward(self, x):
-        """_summary_
+        """computes the model for a given input x
 
         Args:
-            x (_type_): _description_
+            x (torch.Tensor): input tensor, generally as batches of input_size
 
         Returns:
-            _type_: _description_
+            torch.Tensor: reconstructed x for the given input
         """
         result_dict = self.forward_dict(x)
         return result_dict["x_recon"]
 
 
-def load_model(input_size, device):
-    KERNEL_SIZE = 11
-    DIRECTIONS = 5
-    LATENT_DIM = 96  # 64
+def load_model(input_size, device, kernel_size=11, directions=5, latent_dim=96):
+    """_summary_
 
-    # model = VAE((1 if dataset_name == "cxr8" else 3, 224, 224), latent_dim).to(device)
+    Args:
+        input_size (_type_): _description_
+        device (_type_): _description_
+        kernel_size (int, optional): _description_. Defaults to 11.
+        directions (int, optional): _description_. Defaults to 5.
+        latent_dim (int, optional): _description_. Defaults to 96.
+
+    Returns:
+        _type_: _description_
+    """
     model = VAE(
         input_size,
-        init_kernel_size=KERNEL_SIZE,
-        latent_dim=LATENT_DIM,
+        init_kernel_size=kernel_size,
+        latent_dim=latent_dim,
         use_stn=True,
     )
     model = model.to(device)
@@ -333,6 +378,19 @@ def plot_samples(
     recon_loss,
     kldiv_loss,
 ):
+    """_summary_
+
+    Args:
+        model (_type_): _description_
+        start_epoch (_type_): _description_
+        train_loss (_type_): _description_
+        train_count (_type_): _description_
+        batch_idx (_type_): _description_
+        x (_type_): _description_
+        x_recon (_type_): _description_
+        recon_loss (_type_): _description_
+        kldiv_loss (_type_): _description_
+    """
     fig, ax = plt.subplots(3, 5, figsize=(20, 12))
     fig.suptitle(
         f"Epoch {start_epoch+1} Batch {batch_idx} Loss: {train_loss / train_count:.6f} ({recon_loss:.6f}/{kldiv_loss:.6f})"
@@ -365,8 +423,12 @@ def plot_samples(
     plt.close(fig)
 
 
-def train_vae():
-    """perform training of single epoch"""
+def train_vae(device):
+    """perform training of the vae model
+
+    Args:
+        device (torch.Device): device to host training
+    """
     # TODO: move dataset preparation to another function
     data_temp_path = os.environ["DATA_TEMP"]
     root_path = Path(data_temp_path) / "cxr8"
@@ -381,13 +443,7 @@ def train_vae():
     train_loader = DataLoader(train_dataset, batch_size=48, shuffle=True)
     logging.info(f"train_dataset length = {len(train_dataset)}")
 
-    device = torch.device("cuda" if torch.cuda.is_available else "cpu")
-    logging.info(device)
-
     model, optimizer, start_epoch = load_model(input_size, device)
-
-    # TODO: is this still needed?
-    model = model.to(device)
     logging.info(set([p.device for p in model.parameters()]))
 
     # torch.autograd.set_detect_anomaly(True)
@@ -397,31 +453,29 @@ def train_vae():
     train_loss = 0
     train_count = -10
     for batch_idx, batch in enumerate(train_loader):
-        x = batch["image"]
-
-        x = x.to(device)
+        x = batch["image"].to(device)
 
         optimizer.zero_grad()
 
         result_dict = model.forward_dict(x)
-
-        # TODO: get this from the VAE forward_dict
-        v1_weight = torch.tensor(
-            [1.0**1] * 8
-            + [0.5**1] * 8
-            + [0.25**1] * 8
-            + [0.125**1] * 8
-            + [0.06125**1] * 8
-        )
-        logging.info(f"v1_weight = {v1_weight}")
-        result_dict["v1_weight"] = v1_weight.to(device)
+        result_dict["v1_weight"] = result_dict["v1_weight"].to(device)
+        logging.info(f"v1_weight = {result_dict['v1_weight']}")
 
         recon_loss, kldiv_loss, loss = vae_loss(
             recon_loss_metrics=(F.binary_cross_entropy, F.l1_loss),
-            beta=0.5,
+            beta=0.2,
             x=x,
             **result_dict,
         )
+
+        loss.backward()
+        # print(f"loss = {loss}; {'nan' if loss.isnan() else ''}")
+
+        optimizer.step()
+
+        # bit of logic to wait before starting to accumulate loss
+        train_count += 1.0 if train_count != -1.0 else 2.0
+        train_loss = loss.item() + (train_loss if train_count > 0.0 else 0.0)
 
         if train_count % 10 == 9:
             plot_samples(
@@ -435,15 +489,6 @@ def train_vae():
                 recon_loss,
                 kldiv_loss,
             )
-
-        loss.backward()
-        # print(f"loss = {loss}; {'nan' if loss.isnan() else ''}")
-
-        optimizer.step()
-
-        # bit of logic to wait before starting to accumulate loss
-        train_count += 1.0 if train_count != -1.0 else 2.0
-        train_loss = loss.item() + (train_loss if train_count > 0.0 else 0.0)
 
         logging.info(f"Epoch {start_epoch+1}: Batch {batch_idx}")
         logging.info(
@@ -464,15 +509,22 @@ def train_vae():
     logging.info("completed training")
 
 
-def infer_vae(source_dir):
+def infer_vae(device, input_size, source_dir):
+    """_summary_
+
+    Args:
+        device (_type_): _description_
+        input_size (_type_): _description_
+        source_dir (_type_): _description_
+    """
     print(f"inferring images in {source_dir}")
 
-    # TODO: print help
-    vae = VAE((1, 224, 224), 128)
+    model, _, start_epoch = load_model(input_size, device)
+
     print(
         summary(
-            vae,
-            input_size=(37, 1, 224, 224),
+            model,
+            input_size=(37,) + input_size,
             depth=10,
             col_names=[
                 "input_size",
@@ -484,6 +536,9 @@ def infer_vae(source_dir):
             ],
         )
     )
+
+    # TODO: use bokeh to create a figure for inferences
+    logging.warn("TODO: use bokeh to create a figure for inferences")
 
 
 if "__main__" == __name__:
@@ -501,6 +556,7 @@ if "__main__" == __name__:
     args = parser.parse_args()
 
     # TODO: log config via yaml
+    logging.warn("TODO: switch to log config via yaml")
     log_base = datetime.date.today().strftime("%Y%m%d")
     logging.basicConfig(
         filename=f"runs/{log_base}_vae_main.log",
@@ -508,8 +564,11 @@ if "__main__" == __name__:
         level=logging.DEBUG,
     )
 
+    device = torch.device("cuda" if torch.cuda.is_available else "cpu")
+    logging.info(f"torch operations on {device} device")
+
     if args.train:
-        train_vae()
+        train_vae(device)
 
     if args.infer:
-        infer_vae(args.infer)
+        infer_vae(device, (1, 448, 448), args.infer)
